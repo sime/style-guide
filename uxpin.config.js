7cf80c612f2b1266f9f020fd81171b2ec8ce00e7
coderitual <skowronkow@gmail.com>
2020-09-17 14:51:56 +0200

    Add uxpin merge tool and basic webpack config

---
+++ uxpin.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  components: {
+    categories: [
+      {
+        name: 'General',
+        include: ['src/components/buttons/Button.jsx'],
+      },
+    ],
+    webpackConfig: './uxpin.webpack.config.js',
+  },
+  name: 'Brainly Pencil - Style Guide1',
+};
---
+++ uxpin.webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+
+module.exports = {
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  resolve: {
+    modules: [__dirname, 'node_modules'],
+    extensions: ['*', '.js', '.jsx'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        loader: 'svg-react-loader',
+      },
+      {
+        test: /\.(s*)css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2,
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      {
+        loader: 'babel-loader',
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+    ],
+  },
+};
