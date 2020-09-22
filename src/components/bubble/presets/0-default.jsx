69c3cf91b6d284eba294d1ff5ae59136bd259be6
coderitual <skowronkow@gmail.com>
2020-09-22 14:46:35 +0200

    Add bubble preset

---
+++ src/components/bubble/presets/0-default.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Bubble from '../Bubble';
+
+export default (
+  <Bubble uxpId="bubble-1" direction="top" alignment="start">
+    Hi there!! Just wondering if you have any problems with your school work.
+  </Bubble>
+);
