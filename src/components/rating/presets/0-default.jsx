b153bc5843550fba2b2f99ab583f75fe0e7a61fb
coderitual <skowronkow@gmail.com>
2020-09-22 14:54:12 +0200

    Add rating preset

---
+++ src/components/rating/presets/0-default.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Rating from '../Rating';
+
+export default (
+  <Rating
+    uxpId="rating-1"
+    rate={2.4}
+    counterText="34 votes"
+    activeText="Rate!"
+  />
+);
